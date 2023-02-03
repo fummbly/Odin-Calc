2a335fe910920a5cb5ad6b736a963d4a0562198a
fummbly <tstone449@gmail.com>
2023-02-03 13:44:08 -0500

    Finished Logic of the Calculator
    Finished logic in calc file for a functional calculator

--- calc.js
+++ calc.js
@@ -1,66 +1,144 @@
-const operationDisp = document.getElementById('operation')
-const equalsDisp = document.getElementById('equals')
-const numBtns = document.getElementsByClassName('num')
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null;
+let shouldResetScreen = false;
 
-for(let button of numBtns) {
-    button.addEventListener('click', () => {
-        operationDisp.innerHTML += button.dataset.value;
-    })
+
+const numberBtns = document.querySelectorAll('[data-value]')
+const operatorBtns = document.querySelectorAll('[data-operator]')
+
+const equalsBtn = document.getElementById('equal')
+const clearBtn = document.getElementById('clear')
+const deleteBtn = document.getElementById('delete')
+const lastOp  = document.getElementById('lastOp')
+const currOp = document.getElementById('currOp')
+
+window.addEventListener('keydown' , handleKeyboardInput)
+equalsBtn.addEventListener('click', evaluate)
+clearBtn.addEventListener('click', clear)
+deleteBtn.addEventListener('click', deleteNumber)
+
+numberBtns.forEach((button) => 
+    button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operatorBtns.forEach((button) => 
+    button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+function appendNumber(number) {
+    if(currOp.textContent === "0" || shouldResetScreen) 
+        resetScreen()
+    currOp.textContent += number
+}
+
+function resetScreen() {
+    currOp.textContent = ""
+    shouldResetScreen = false;
+}
+
+function clear() {
+    currOp.textContent = "0"
+    lastOp.textContent = ""
+    firstOperand = ""
+    secondOperand = ""
+    currentOperation = null;
+}
+
+function appendPoint() {
+    if (shouldResetScreen) resetScreen()
+    if (currOp.textContent === "")
+        currOp.textContent = "0"
+    if (currOp.textContent.includes('.')) return
+    currOp.textContent += '.'
 }
 
 
+function deleteNumber() {
+    currOp.textContent = currOp.textContent
+        .toString()
+        .slice(0, -1);
+}
 
 
-function add(a, b) {
-    if(!Number.isInteger(a) || !Number.isInteger(b)) {
-        console.log("Not an integer")
+function setOperation(operator) {
+    if (currentOperation !== null) evaluate()
+    firstOperand = currOp.textContent
+    currentOperation = operator
+    lastOp.textContent = `${firstOperand} ${currentOperation}`
+    shouldResetScreen = true
+}
+
+function evaluate() {
+    if (currentOperation === null || shouldResetScreen) return
+    if (currentOperation === "÷" && currOp.textContent === "0") {
+        alert("You can't divide by 0!")
         return
     }
+    secondOperand = currOp.textContent
+    currOp.textContent = roundResult(
+        operate(currentOperation, firstOperand, secondOperand)
+    )
+    lastOp.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
+    currentOperation = null;
+}
 
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000
+}
+
+function handleKeyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
+    if (e.key === ".") appendPoint()
+    if (e.key === "=" || e.key === 'Enter') evaluate()
+    if (e.key === 'Backspace') deleteNumber()
+    if (e.key === "Escape") clear()
+    if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
+        setOperation(convertOperator(e.key)) 
+}
+
+function convertOperator(keyOperator) {
+    if (keyOperator === "/") return "÷"
+    if (keyOperator === "*") return "*"
+    if (keyOperator === "+") return "+"
+    if (keyOperator === "-") return "-"
+}
+
+function add(a, b) {
     return a + b;
 }
 
 function sub(a, b) {
-    if(!Number.isInteger(a) || !Number.isInteger(b)) {
-        console.log("Not an integer")
-        return
-    }
 
     return a - b;
 
 }
 
 function mult(a, b) {
-    if(!Number.isInteger(a) || !Number.isInteger(b)) {
-        console.log("Not an integer")
-        return
-    }
 
     return a * b;
 }
 
 function div(a, b) {
-    if(!Number.isInteger(a) || !Number.isInteger(b)) {
-        console.log("Not an integer")
-        return
-    }
 
     return a / b;
 }
 
 function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
     var result;
     switch(operator) {
-        case "add":
+        case "+":
             result = add(a, b);
             break;
-        case "sub":
+        case "-":
             result = sub(a, b)
             break;
-        case "mult":
+        case "*":
             result = mult(a, b)
             break;
-        case "div":
+        case "÷":
             result = div(a, b);
             break;
         default:
